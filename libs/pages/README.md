903f22d4757f12621fab964c8ef0110888d4cc47
Denis Zholob <dddgamer3x@gmail.com>
2021-11-12 22:17:17 -0500

    Add new Pages Library

---
+++ libs/pages/README.md
@@ -0,0 +1,7 @@
+# Pages Library
+Contains top level page views
+
+_This library was generated with [Nx](https://nx.dev)._
+
+## Actions
+* Run `nx test pages` to execute the unit tests.
---
+++ libs/pages/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'pages',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/libs/pages',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
